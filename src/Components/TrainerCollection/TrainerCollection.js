4b375e7e706fe5d1b643e0aa429bb91e82c8e7ec
mathyou <62043626+mathyougit@users.noreply.github.com>
2020-08-17 13:18:09 -0400

    new sort feature built

--- src/Components/TrainerCollection/TrainerCollection.js
+++ src/Components/TrainerCollection/TrainerCollection.js
@@ -3,27 +3,70 @@ import './TrainerCollection.css'
 
 const TrainerCollection = (props) => {
   const [pokemons, setPokemons] = useState(props.pokemons);
+  const [sortType, setSortType] = useState('nameAZ');
 
-  const handleSort = (sortType) => {
+  const handleSortChange = e => {
+    setSortType(e.target.value);
+  }
+
+  const handleSort = () => {
+    const sortedPokemons = [...pokemons];
+    
     const compareNameAZ = (a,b) => {
       return (a['name'].toLowerCase() > b['name'].toLowerCase()) ? 1 : (a['name'].toLowerCase() < b['name'].toLowerCase()) ? -1 : 0;
     }
     const compareNameZA = (a,b) => {
       return (a['name'].toLowerCase() > b['name'].toLowerCase()) ? -1 : (a['name'].toLowerCase() < b['name'].toLowerCase()) ? 1 : 0;
     }
-    const sortedPokemons = [...pokemons];
-    if (sortType === 'name/a-z') {
+    const comparePokeIdAsc = (a,b) => {
+      return (a['_id'] > b['_id']) ? 1 : (a['_id'] < b['_id']) ? -1 : 0;
+    }
+    const comparePokeIdDsc = (a,b) => {
+      return (a['_id'] > b['_id']) ? -1 : (a['_id'] < b['_id']) ? 1 : 0;      
+    }
+    const compareRarityAsc = (a,b) => {
+      return (a['rarity'] > b['rarity']) ? 1 : (a['rarity'] < b['rarity']) ? -1 : 0;      
+    }
+    const compareRarityDsc = (a,b) => {
+      return (a['rarity'] > b['rarity']) ? -1 : (a['rarity'] < b['rarity']) ? 1 : 0;           
+    }
+
+    if (sortType === 'nameAZ') {
       sortedPokemons.sort( compareNameAZ )
     } 
-    if (sortType === 'name/z-a') {
+    if (sortType === 'nameZA') {
       sortedPokemons.sort( compareNameZA )
     }
+    if (sortType === 'pokeIdAsc') {
+      sortedPokemons.sort( comparePokeIdAsc )
+    }
+    if (sortType === 'pokeIdDsc') {
+      sortedPokemons.sort( comparePokeIdDsc )
+    }
+    if (sortType === 'rarityAsc') {
+      sortedPokemons.sort( compareRarityAsc )
+    }
+    if (sortType === 'rarityDsc') {
+      sortedPokemons.sort( compareRarityDsc )
+    }
+
     setPokemons(sortedPokemons)
   }
 
   return(
     <div className='trainer-collection'>
       Have a look at your Pokemon! <br />
+      <select 
+        value={sortType} 
+        onChange={handleSortChange}>
+        <option value='nameAZ'>By Name; A-Z</option>
+        <option value='nameZA'>By Name; Z-A</option>
+        <option value='pokeIdAsc'>By Pokedex Id; Ascending</option>
+        <option value='pokeIdDsc'>By Pokedex Id; Descending</option>
+        <option value='rarityAsc'>By Rarity; Ascending</option>
+        <option value='rarityDsc'>By Rarity; Descending</option>
+      </select>
+
       <button className='trainer-collection-sort'
         onClick={handleSort}
       >
